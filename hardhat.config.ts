ae6086b2dc0ecf2981b4186a09c0b6189d37fb9a
Emir Soyturk <emirsytrk@gmail.com>
2024-07-13 14:15:15 +0300

    Deployment Script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,20 +1,59 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import dotenv from "dotenv";
+dotenv.config();
+
+const private_key = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
-    scrollSepolia: {
-      url: "https://sepolia-rpc.scroll.io/" || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {},
+    scroll_testnet: {
+      url: "https://sepolia-rpc.scroll.io/",
+      accounts: [private_key],
     },
-    fhenixTestnet: {
+    fhenix_testnet: {
       url: "https://api.testnet.fhenix.zone:7747",
-      accounts:
-      process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    }
+      accounts: [private_key],
+    },
+    sepolia: {
+      url: `https://rpc.notadegen.com/eth/sepolia`,
+      accounts: [private_key],
+    },
+    arbitrum_sepolia: {
+      url: `https://sepolia-rollup.arbitrum.io/rpc`,
+      chainId: 421614,
+      accounts: [private_key],
+    },
+    linea_goerli: {
+      url: `https://rpc.goerli.linea.build`,
+      chainId: 59140,
+      accounts: [private_key],
+    },
   },
+  // etherscan: {
+  //   apiKey: process.env.SCAN_API_KEY,
+  //   customChains: [
+  //     {
+  //       network: "scroll_testnet",
+  //       chainId: 534351,
+  //       urls: {
+  //         apiURL: "https://api-sepolia.scrollscan.com/api",
+  //         browserURL: "https://sepolia.scrollscan.dev/",
+  //       },
+  //     },
+  //     {
+  //       network: "linea_goerli",
+  //       chainId: 59140,
+  //       urls: {
+  //         apiURL: "https://api-testnet.lineascan.build/api",
+  //         browserURL: "https://goerli.lineascan.build/",
+  //       },
+  //     },
+  //   ],
+  // },
 };
 
 export default config;
