5f369375337773f04513efba4052cc2771c8757f
d0x471b <0x471@protonmail.com>
2024-07-14 02:19:37 +0300

    update relayer worker

--- scripts/relayer-worker.ts
+++ scripts/relayer-worker.ts
@@ -3,53 +3,25 @@ import dotenv from "dotenv";
 import { exec } from 'child_process';
 dotenv.config();
 
-const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/scroll_sepolia_testnet");
+const provider = new ethers.WebSocketProvider(process.env.SCROLL_WSS || "");
 
 const contractAddress = process.env.MAILBOX_ADDRESS || "";
 const privateKey = process.env.PRIVATE_KEY || "";
 
-const filter = {
-    address: contractAddress,
-    topics: [ethers.id("DispatchId(bytes32)")]
-};
+const iface = new ethers.Interface([
+    "event DispatchId(bytes32 indexed id)"
+]);
 
-async function fetchLogs() {
-    try {
-        const logs = await provider.getLogs(filter);
-        if (logs.length === 0) {
-            console.log("No logs found.");
-        }
-        logs.forEach(log => {
-            console.log("DispatchId:", log.topics[1]);
+const contract = new ethers.Contract(contractAddress, iface, provider);
 
-            const command = `hyperlane status --relay --id ${log.topics[1]} --origin scrollsepolia --destination fhenix --registry ./scripts/hyperlane-registry -k ${privateKey}`;
-            exec(command, (error, stdout, stderr) => {
-                // if (error) {
-                //     console.error(`Command failed: ${error.message}`);
-                //     return;
-                // }
-                console.log(`Command output: ${stdout}`);
-                console.error(`Command error (if any): ${stderr}`);
-            });
-        });
-    } catch (error) {
-        console.error("Error fetching logs:", error);
-    }
-}
+console.log("🏁 Worker started listening")
+contract.on("DispatchId", (id) => {
+    console.log("🤠 DispatchId:", id);
 
-async function listenForEvents() {
-    console.log("Listening to RadioFM 🎸");
-
-    while (true) {
-        try {
-            await fetchLogs();
-        } catch (error) {
-            console.error("Error in fetchLogs:", error);
-        }
-
-        // Introduce a delay between each fetch to avoid overwhelming the node
-        await new Promise(resolve => setTimeout(resolve, 5000));
-    }
-}
-
-listenForEvents().catch(error => console.error("Error in listenForEvents:", error));
+    const command = `hyperlane status --relay --id ${id} --origin scrollsepolia --destination fhenix --registry ./scripts/hyperlane-registry -k ${privateKey}`;
+    exec(command, (error, stdout, stderr) => {
+        console.log("🔫 Executing Hyperlane!");
+        console.log(`Command output: ${stdout}`);
+        console.error(`Command error (if any): ${stderr}`);
+    });
+});
