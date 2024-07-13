ff254273a432841a85cfc2f7f3e104286c97fece
d0x471b <0x471@protonmail.com>
2024-07-13 13:23:50 +0300

    🆒 Integrate ad matrix

--- contracts/AdContract.sol
+++ contracts/AdContract.sol
@@ -1,17 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { IMailbox } from "./IMailbox.sol";
+import {IMailbox} from "./IMailbox.sol";
 
 contract AdContract {
     struct Ad {
-        address payable advertiser;  // Address of the advertiser
-        string adTitle;              // Title of the ad
-        string adContent;            // Content or description of the ad
-        uint256 createdAt;           // Timestamp when the ad was created
-        uint256 expiresAt;           // Timestamp when the ad expires
-        uint256 budget;              // Budget allocated for the ad
-        bool isActive;               // Flag indicating if the ad is active or not
+        address payable advertiser; // Address of the advertiser
+        string adTitle; // Title of the ad
+        string adContent; // Content or description of the ad
+        uint256 createdAt; // Timestamp when the ad was created
+        uint256 expiresAt; // Timestamp when the ad expires
+        uint256 budget; // Budget allocated for the ad
+        bool isActive; // Flag indicating if the ad is active or not
+        bool[5] adVector;
     }
 
     mapping(uint256 => Ad) public ads;
@@ -19,9 +20,16 @@ contract AdContract {
     uint256 public constant COST_PER_SECOND = 0.01 ether; // Cost per second for the ad
     address public owner;
     IMailbox public mailbox; // Add the mailbox instance
+    address public recipient;
 
     // Event emitted when a new ad is created
-    event AdCreated(uint256 indexed adId, address indexed advertiser, string adTitle, uint256 expiresAt, uint256 budget);
+    event AdCreated(
+        uint256 indexed adId,
+        address indexed advertiser,
+        string adTitle,
+        uint256 expiresAt,
+        uint256 budget
+    );
 
     // Constructor to set the owner of the contract and mailbox address
     constructor(address _mailboxAddress) {
@@ -34,8 +42,9 @@ contract AdContract {
         string memory _adTitle,
         string memory _adContent,
         uint256 _durationInSeconds,
-        uint256 _budget
-    ) external payable {
+        uint256 _budget,
+        bool[5] memory _adVector
+    ) external payable returns (bytes32) {
         uint256 cost = _durationInSeconds * COST_PER_SECOND;
         require(msg.value >= cost, "Insufficient payment for the ad duration");
 
@@ -46,28 +55,50 @@ contract AdContract {
             createdAt: block.timestamp,
             expiresAt: block.timestamp + _durationInSeconds,
             budget: _budget,
-            isActive: true
+            isActive: true,
+            adVector: _adVector
         });
 
-        emit AdCreated(nextAdId, msg.sender, _adTitle, block.timestamp + _durationInSeconds, _budget);
+        emit AdCreated(
+            nextAdId,
+            msg.sender,
+            _adTitle,
+            block.timestamp + _durationInSeconds,
+            _budget
+        );
 
-        mailbox.dispatch{value: msg.value}(
+        bytes memory bytesArray = new bytes(_adVector.length);
+        for (uint i = 0; i < _adVector.length; i++) {
+            if (_adVector[i]) {
+                bytesArray[i] = 0x01;
+            } else {
+                bytesArray[i] = 0x00;
+            }
+        }
+
+        bytes32 id = mailbox.dispatch{value: msg.value}(
             97, // destinationDomain
-            0x0000000000000000000000006489d13AcAd3B8dce4c5B31f375DE4f9451E7b38, // recipientAddress
-            bytes("Hello, world") // messageBody
+            bytes32(uint256(uint160(recipient))), // recipientAddress
+            bytes(bytesArray) // messageBody
         );
 
         // Increment the nextAdId
         nextAdId++;
+        return id;
     }
 
     // Function to retrieve the cost for a given duration
-    function calculateCost(uint256 _durationInSeconds) external pure returns (uint256) {
+    function calculateCost(
+        uint256 _durationInSeconds
+    ) external pure returns (uint256) {
         return _durationInSeconds * COST_PER_SECOND;
     }
 
     // Function to extend an ad's duration
-    function extendAd(uint256 _adId, uint256 _additionalDurationInSeconds) external payable {
+    function extendAd(
+        uint256 _adId,
+        uint256 _additionalDurationInSeconds
+    ) external payable {
         require(_adId < nextAdId, "Invalid ad ID");
 
         Ad storage ad = ads[_adId];
@@ -75,7 +106,10 @@ contract AdContract {
 
         uint256 newExpiresAt = ad.expiresAt + _additionalDurationInSeconds;
         uint256 cost = _additionalDurationInSeconds * COST_PER_SECOND;
-        require(msg.value >= cost, "Insufficient payment to extend the ad duration");
+        require(
+            msg.value >= cost,
+            "Insufficient payment to extend the ad duration"
+        );
 
         ad.expiresAt = newExpiresAt;
 
@@ -90,16 +124,22 @@ contract AdContract {
         require(_adId < nextAdId, "Invalid ad ID");
 
         Ad storage ad = ads[_adId];
-        require(ad.advertiser == msg.sender, "Only advertiser can deactivate the ad");
-        
+        require(
+            ad.advertiser == msg.sender,
+            "Only advertiser can deactivate the ad"
+        );
+
         ad.isActive = false;
     }
 
     // Withdraw funds from the contract (for contract owner)
     function withdraw(uint256 _amount) external {
         require(msg.sender == owner, "Only contract owner can withdraw funds");
-        require(address(this).balance >= _amount, "Insufficient contract balance");
-        
+        require(
+            address(this).balance >= _amount,
+            "Insufficient contract balance"
+        );
+
         payable(msg.sender).transfer(_amount);
     }
 
